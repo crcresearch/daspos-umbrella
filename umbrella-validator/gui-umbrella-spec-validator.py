050940de8775d200e9bb3782813d47119fd8f9a9
Rob Hostetler <Macainian123@hotmail.com>
2015-12-18 01:53:15 -0500

    Initial library commit.

---
+++ umbrella-validator/gui-umbrella-spec-validator.py
@@ -0,0 +1,334 @@
+# Copyright (C) 2015, University of Notre Dame
+# All rights reserved
+import hashlib
+import json
+import urllib2
+from Tkinter import *
+from os import path
+from tkFileDialog import askopenfilename
+
+try:
+    ttk_imported = True
+    from ttk import Button, Style
+except ImportError:
+    print "Umbrella Spec Validator: ttk module not installed"
+    ttk_imported = False
+
+class ValidatorUI(Frame):
+
+    def __init__(self, parent):
+        Frame.__init__(self, parent)
+
+        self.parent = parent
+        self.width = 400
+        self.height = 200
+        self.center_window()
+        self.msg_str_line1 = StringVar()
+        self.msg_str_line2 = StringVar()
+        self.msg_str_line3 = StringVar()
+        self.errors = dict()
+        self.err_flag = False
+        self.filename_entry = None
+        self.filename_str = StringVar()
+        self.err_popup = None
+
+        self.init_ui()
+
+    def init_ui(self):
+        self.parent.title("Umbrella Spec Validator")
+
+        if ttk_imported:
+            self.parent.resizable(0, 0)
+            self.style = Style()
+
+            try:
+                self.style.theme_use("vista")
+            except _tkinter.TclError:
+                self.style.theme_use("default")
+        else:
+            self.parent.resizable(1, 0)
+
+        self.pack(fill=BOTH, expand=1)
+
+        self.init_entry()
+        self.init_bttns()
+
+    def center_window(self):
+        sw = self.parent.winfo_screenwidth()
+        sh = self.parent.winfo_screenheight()
+
+
+        x = (sw - self.width)/2
+        y = (sh - self.height)/2
+        self.parent.geometry('%dx%d+%d+%d' % (self.width, self.height, x, y))
+
+    def init_bttns(self):
+        bttn_frame = Frame(self.parent)
+
+        Button(bttn_frame, text='Validate', command=self.check_validity).pack(side=LEFT, pady=10, padx=3)
+        Button(bttn_frame, text='Exit', command=root.quit).pack(side=LEFT, pady=10, padx=3)
+
+        bttn_frame.pack(side=BOTTOM)
+
+    def init_entry(self):
+        # Filepath entry field
+        entry_frame = Frame(self.parent)
+
+        # Message Label
+        Label(entry_frame, text="", textvariable=self.msg_str_line1, width=self.width, font="Arial 10 bold").pack(side=TOP)
+        Label(entry_frame, text="", textvariable=self.msg_str_line2, width=self.width).pack(side=TOP)
+        Label(entry_frame, text="", textvariable=self.msg_str_line3, width=self.width).pack(side=TOP)
+
+        self.update_label("Welcome to the Umbrella Spec Validator!")
+
+        # File Entry
+        Label(entry_frame, text='Spec File Path: ').pack(side=LEFT, padx=10, pady=10)
+        self.filename_entry = Entry(entry_frame, textvariable=self.filename_str, width=33)
+        self.filename_entry.pack(side=LEFT, pady=20)
+        Button(entry_frame, text="Search", command=self.browse_for_spec).pack(side=LEFT, padx=10, pady=10)
+
+        entry_frame.pack(side=TOP)
+
+    def browse_for_spec(self):
+        Tk().withdraw()
+        filename = askopenfilename()
+        self.filename_str.set(filename)
+
+    def popup_err(self):
+        # Initialize new popup window
+        self.err_popup = Tk()
+        self.err_popup.title("Errors Found")
+
+        # Add Scrollbar
+        scrollbar = Scrollbar(self.err_popup)
+        scrollbar.pack(side=RIGHT, fill=Y)
+
+        # Initialize Error Messages
+        err_count = 0
+        max_len = 0
+        listbox = Listbox(self.err_popup)
+        listbox.pack(side=LEFT, fill=BOTH, expand=1)
+
+        for filename, source_list in self.errors.iteritems():
+            filename_written = False
+            for source, error_list in source_list.iteritems():
+                if len(error_list) is not 0:
+                    err_count += len(error_list)
+                    file_line = " " + filename + ":"
+                    source_line = "    " + source + ":"
+
+                    # Write filename
+                    if not filename_written:
+                        filename_written = True
+                        listbox.insert(END, "\n")
+                        listbox.insert(END, file_line)
+
+                    # Write url
+                    listbox.insert(END, source_line)
+
+                    # Write Errors
+                    for err_line in error_list:
+                        listbox.insert(END, err_line)
+
+                    # Update required width of window
+                    if max_len < max(len(file_line), len(source_line), len(err_line)):
+                        max_len = max(len(file_line), len(source_line), len(err_line)) + 5
+
+        # Write Title
+        listbox.insert(0, " " + str(err_count) + " ERRORS FOUND:")
+
+        # Add Error Message
+        listbox.config(width=max_len, height=20)
+        listbox.config(yscrollcommand=scrollbar.set)
+        scrollbar.config(command=listbox.yview)
+
+        # Pop up the window
+        self.err_popup.mainloop()
+
+        # Reset the error dictionary and flag
+        self.errors = dict()
+        self.err_flag = False
+
+    def calculate_md5(self, url, data):
+        filename = data["filename"]
+        url_index = data["sources"].index(url)
+        reported_size = data["size"]
+
+        try:
+            remote = urllib2.urlopen(url)
+        except urllib2.HTTPError:
+            self.err_flag = True
+            self.errors[filename][url].append("       HTTP Error 404: Not Found")
+            return None
+
+        try:
+            f_size = float(remote.headers['content-length'])
+        except KeyError:
+            f_size = reported_size
+
+        hash = hashlib.md5()
+        size_downloaded = 0.0
+        size_prev_downloaded = float(f_size*url_index)
+        tot_size_to_download = float(f_size*len(data["sources"]))
+
+        while True:
+            data = remote.read(10240)
+            if not data:
+                percent_downloaded = int((f_size + size_prev_downloaded)/tot_size_to_download * 100)
+                self.update_label("Validating MD5 checksums...", filename + ": " + str(percent_downloaded) + "%")
+                break
+            else:
+                size_downloaded += 10240.0
+
+            hash.update(data)
+            percent_downloaded = int((size_downloaded + size_prev_downloaded)/ tot_size_to_download * 100)
+            self.update_label("Validating MD5 checksums...", filename + ": " + str(percent_downloaded) + "%")
+
+        if int(f_size) != int(reported_size):
+            self.err_flag = True
+            self.errors[filename][url].append("       Incorrect file size. True size: " + str(f_size) + " B")
+
+        return hash.hexdigest()
+
+    def update_label(self, l1, l2=None, l3=None):
+        self.msg_str_line1.set(" ")
+        self.msg_str_line2.set(" ")
+        self.msg_str_line3.set(" ")
+
+        if l1:
+            self.msg_str_line1.set(l1)
+        if l2:
+            self.msg_str_line2.set(l2)
+        if l3:
+            self.msg_str_line3.set(l3)
+
+        self.parent.update_idletasks()
+
+    def truncate_path(self, path, size=36):
+        trunc_beginning = str()
+        trunc_end = str()
+        directory_list = path.split("/") if "/" in path else path.split("\\")
+
+        char_ct = 0
+        i = 0
+
+        if len(path) > size:
+            while char_ct < size:
+                trunc_beginning += directory_list[i] + "/"
+
+                if directory_list[-(i+1)] is not directory_list[i]:
+                    trunc_end = "/" + directory_list[-(i+1)] + trunc_end
+                    i += 1
+                    char_ct = len(trunc_beginning) + 3 + len(trunc_end)
+                else:
+                    break
+
+            return trunc_beginning + "... " + trunc_end
+        else:
+            return path
+
+    def open_file(self, filename):
+        if len(filename.strip()) is 0:
+            return None
+
+        # Incorrect File Path
+        if not path.isfile(filename):
+            self.update_label("File not found.")
+
+            return None
+
+        # Open File
+        with open(str(filename)) as fp:
+            try:
+                spec = json.load(fp)
+            # Can't Load the Spec
+            except ValueError:
+                self.update_label("Can't load file %s" % self.truncate_path(filename))
+                self.filename_entry.delete(0, END)
+                spec = None
+
+        # Can't Load the Spec
+        if spec is None:
+            self.update_label("Can't load file %s" % self.truncate_path(filename))
+            self.filename_entry.delete(0, END)
+
+        return spec
+
+    def check_validity(self):
+        # Open Spec
+        filename = self.filename_entry.get()
+        spec = self.open_file(filename)
+        if spec is None:
+            return
+
+        self.update_label("Validating...")
+
+        # Read Spec Contents
+        files = list()
+        missing = list()
+        if "package_manager" in spec:
+            for name, config in spec["package_manager"]["config"].iteritems():
+                files.append({"filename": name,
+                              "sources": config["source"],
+                              "md5": config["checksum"],
+                              "size": config["size"]})
+
+        if "software" in spec:
+            for name, software in spec["software"].iteritems():
+                files.append({"filename": name,
+                              "sources": software["source"],
+                              "md5": software["checksum"],
+                              "size": software["size"]})
+        if "data" in spec:
+            for name, data in spec["data"].iteritems():
+                files.append({"filename": name,
+                              "sources": data["source"],
+                              "md5": data["checksum"],
+                              "size": data["size"]})
+
+        if "os" in spec:
+            files.append({"filename": spec["os"]["name"],
+                          "sources": spec["os"]["source"],
+                          "md5": spec["os"]["checksum"],
+                          "size": spec["os"]["size"]})
+        else:
+            missing.append("os")
+
+        if "hardware" not in spec:
+            missing.append("hardware")
+
+        if "kernel" not in spec:
+            missing.append("kernel")
+
+
+
+        if len(missing) is not 0:
+            self.update_label("File is not umbrella spec:", self.truncate_path(filename), "Missing " + ", ".join(missing))
+            self.filename_entry.delete(0, END)
+            return
+
+        for data in files:
+            filename = data["filename"]
+            self.errors[filename] = dict()
+            for url in data["sources"]:
+                self.errors[filename][url] = list()
+                md5 = self.calculate_md5(url, data)
+                if md5 and md5 != data["md5"]:
+                    self.err_flag = True
+                    self.errors[filename][url].append("       Hash incorrect. Given: " + data["md5"] + ", Calculated: " + md5)
+
+        if not self.err_flag:
+            self.update_label("Validation successful")
+            self.parent.bell()
+
+        else:
+            self.update_label("Validation unsuccessful")
+            self.filename_entry.delete(0, END)
+            self.popup_err()
+
+if __name__ == "__main__":
+    root = Tk()
+    root.geometry("250x150+300+300")
+    app = ValidatorUI(root)
+    root.mainloop()
+
---
+++ umbrella-validator/validator.py
@@ -0,0 +1,171 @@
+# Copyright (C) 2016, University of Notre Dame
+# All rights reserved
+import hashlib
+import json
+import urllib2
+
+CONFIG = 'config'
+FILE_NAME = 'name'
+URL_SOURCES = 'source'
+MD5 = 'checksum'
+FILE_SIZE = 'size'
+UNCOMPRESSED_FILE_SIZE = 'uncompressed_size'
+FILE_FORMAT = 'format'
+DOWNLOAD_CHUNK_SIZE = 10240
+
+SPECIFICATION_COMPONENTS = {
+    'hardware': {
+        'required': True,
+        'has_files': False,
+    },
+    'kernel': {
+        'required': True,
+        'has_files': False,
+    },
+    'os': {
+        'required': True,
+        'has_files': True,
+    },
+    'package_manager': {
+        'required': False,
+        'has_files': True,
+    },
+    'software': {
+        'required': False,
+        'has_files': True,
+    },
+    'data': {
+        'required': False,
+        'has_files': True,
+    },
+}
+
+
+def validate(specification_file):
+    # Open Specification
+    if isinstance(specification_file, file):
+        specification_json = json.load(specification_file)
+    elif isinstance(specification_file, (str, unicode)):
+        specification_json = json.loads(specification_file)
+    elif isinstance(specification_file, dict):
+        specification_json = specification_file
+    else:
+        raise ValueError('Specification file must be an open file, json in string form, or a python dictionary')
+
+    # Initialize lists
+    file_infos = []
+    read_specifications = []
+    duplicate_specifications = []
+    missing_specifications = []
+    unknown_specifications = []
+
+    # Cycle through all of the known specification components
+    for component_name, component in SPECIFICATION_COMPONENTS.iteritems():
+        if component_name in read_specifications:  # Have this component already been read, if so, it is a duplicate
+            duplicate_specifications.append(component_name)
+        elif component_name in specification_json:  # Is this component in the file, if so, check it
+            read_specifications.append(component_name)
+
+            if component['has_files']:
+                # Package Manager is set up slightly differently
+                if component_name == 'package_manager':
+                    component_file_info = specification_json[component_name][CONFIG]
+                else:
+                    component_file_info = specification_json[component_name]
+
+                # Loop through each files info
+                for name, file_info in component_file_info.iteritems():
+                    # OS has its name inside its general info section
+                    if component_name == 'os':
+                        file_name = file_info[FILE_NAME]
+                    else:
+                        file_name = name
+
+                    file_infos.append({
+                        FILE_NAME: file_name,
+                        URL_SOURCES: file_info[URL_SOURCES],
+                        MD5: file_info[MD5],
+                        FILE_SIZE: file_info[FILE_SIZE]
+                    })
+        elif component['required']:  # Is this component required (and we didn't have it), if so, it is missing
+            missing_specifications.append(component_name)
+        else:  # Is this component not in the list of possible components, if so, it is unknown
+            unknown_specifications.append(component_name)
+
+    for file_info in file_infos:
+        file_name = file_info[FILE_NAME]
+
+        for url in file_info[URL_SOURCES]:
+            md5 = get_md5(url, file_info[FILE_SIZE])
+
+            if md5 and md5 != file_info[MD5]:
+                pass
+
+
+def get_md5(the_file_or_url, file_size, call_back_function=None, *args):
+    if isinstance(the_file_or_url, file):
+        return calculate_md5_via_file(the_file_or_url, file_size, call_back_function, *args)
+    elif isinstance(the_file_or_url, (str, unicode)):
+        return calculate_md5_via_url(the_file_or_url, file_size, call_back_function, *args)
+    else:
+        raise ValueError('the_file_or_url must be a file or a string form of a url')
+
+
+def calculate_md5_via_url(url, supposed_file_size, call_back_function, *args):
+    if not isinstance(url, (str, unicode)):
+        raise ValueError('Url must be in string form ')
+
+    try:
+        remote = urllib2.urlopen(url)
+    except urllib2.HTTPError:
+        raise ValueError('404? Need to put this in an error log.')
+
+    # Get the file_size from the website. Some websites (old ones) may not give this information
+    try:
+        file_size = int(remote.headers['content-length'])
+    except KeyError:
+        file_size = None
+
+    return calculate_md5(remote, file_size, supposed_file_size, call_back_function, *args)
+
+
+def calculate_md5_via_file(the_file, file_size, call_back_function, *args):
+    if not isinstance(the_file, file):
+        raise ValueError('the_file must be an open file ')
+
+    return calculate_md5(the_file, file_size, file_size, call_back_function, *args)
+
+
+def calculate_md5(data_source, actual_file_size, supposed_file_size, call_back_function, *args):
+    bytes_downloaded = 0
+    md5 = hashlib.md5()
+
+    if actual_file_size is None:
+        percent_processed = -1
+        call_back_function(percent_processed, *args)
+
+    while True:
+        data = data_source.read(DOWNLOAD_CHUNK_SIZE)
+
+        # There was no more data to read
+        if not data:
+            break
+
+        bytes_downloaded += len(data)
+
+        if actual_file_size:
+            percent_processed = float(bytes_downloaded / actual_file_size * 100)
+
+            if percent_processed > 10:
+                call_back_function(percent_processed, *args)
+
+        md5.update(data)
+
+    if bytes_downloaded != supposed_file_size:
+        raise ValueError('File size mismatch. Need to put this in a log.')
+
+    return md5.hexdigest()
+
+
+if __name__ == "__main__":
+    pass
