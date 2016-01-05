7e90a8ac4fdcdaac97ba0079dc861070fdd6d306
Alexander Vyushkov <baguage@gmail.com>
2016-01-05 18:22:37 -0500

    Added simple setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='daspos-umbrella',
+    version='0.1.0',
+    packages=['umbrella'],
+    url='',
+    license='',
+    author='University of Notre Dame',
+    author_email='CRCSupport@nd.edu',
+    description=''
+)
