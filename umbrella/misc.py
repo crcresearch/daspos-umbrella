6db2f2e82cabcd0974bcf7d995ede156285c8a71
Alexander Vyushkov <baguage@gmail.com>
2016-01-08 20:16:45 -0500

    Added copyright statements

--- umbrella/misc.py
+++ umbrella/misc.py
@@ -1,5 +1,20 @@
-# Copyright (C) 2015-2016, University of Notre Dame
-# All rights reserved
+# This file is part of the daspos-umbrella package.
+#
+# For copyright and licensing information about this package, see the
+# NOTICE.txt and LICENSE.txt files in its top-level directory; they are
+# available at https://github.com/crcresearch/daspos-umbrella
+#
+# Licensed under the MIT License (MIT);
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://opensource.org/licenses/MIT
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import hashlib
 
 
